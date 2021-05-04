4bc6915dd2fca847f595dc1ff0eb399c9cf87b0c
natalia-mus <natalia.muskala1ab1@gmail.com>
2021-05-04 17:54:37 +0200

    Bottom navigation - switching between fragments

--- app/src/main/java/com/example/myentertainment/MainActivity.kt
+++ app/src/main/java/com/example/myentertainment/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.myentertainment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigation = findViewById(R.id.bottomNavigation_bar)
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.mainActivity_frameLayout, MoviesFragment())
+            commit()
+        }
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item_movies -> changeCurrentFragment(MoviesFragment())
+                R.id.item_books -> changeCurrentFragment(BooksFragment())
+                R.id.item_games -> changeCurrentFragment(GamesFragment())
+                R.id.item_music -> changeCurrentFragment(MusicFragment())
+            }
+            true
+        }
     }
+
+
+    private fun changeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.mainActivity_frameLayout, fragment)
+            commit()
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/myentertainment/MoviesFragment.kt
@@ -0,0 +1,18 @@
+package com.example.myentertainment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MoviesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movies, container, false)
+    }
+}
