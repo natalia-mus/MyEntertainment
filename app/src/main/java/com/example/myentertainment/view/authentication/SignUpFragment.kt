79a17b1be25121e96ed11669622b610e7b98662a
natalia-mus <natalia.muskala1ab1@gmail.com>
2021-05-05 21:28:46 +0200

    Validation

--- app/src/main/java/com/example/myentertainment/view/authentication/SignUpFragment.kt
+++ app/src/main/java/com/example/myentertainment/view/authentication/SignUpFragment.kt
@@ -1,15 +1,19 @@
 package com.example.myentertainment.view.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.myentertainment.R
+import com.example.myentertainment.`object`.ValidationObject
+import com.example.myentertainment.view.main.MainActivity
 import com.example.myentertainment.viewmodel.SignUpFragmentViewModel
 
 class SignUpFragment : Fragment() {
@@ -24,6 +28,8 @@ class SignUpFragment : Fragment() {
 
     private lateinit var email: String
     private lateinit var password: String
+
+    private var message = ""
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,6 +58,8 @@ class SignUpFragment : Fragment() {
 
     private fun setObservers() {
         viewModel.loading.observe(this, { updateView(it) })
+        viewModel.validationResult.observe(this, { validationResult(it) })
+        viewModel.signingUpStatus.observe(this, { signingUpResult(it) })
     }
 
     private fun updateView(loading: Boolean) {
@@ -60,5 +68,34 @@ class SignUpFragment : Fragment() {
         } else {
             loadingSection.visibility = View.INVISIBLE
         }
+    }
+
+    private fun validationResult(validationResult: Int) {
+        when (validationResult) {
+            ValidationObject.EMPTY_VALUES -> {
+                message = "Enter both e-mail and password"
+            }
+            ValidationObject.PASSWORD_TOO_SHORT -> {
+                message = "Your password should be at least 6 characters long"
+            }
+            ValidationObject.INVALID_EMAIL -> {
+                message = "Your e-mail address is invalid"
+            }
+        }
+        toast(message)
+    }
+
+    private fun signingUpResult(signingUpStatus: Boolean) {
+        if (signingUpStatus) {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        } else {
+            message = "Unfortunately, user can not be created"
+            toast(message)
+        }
+    }
+
+    private fun toast(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/example/myentertainment/viewmodel/SignUpFragmentViewModel.kt
+++ app/src/main/java/com/example/myentertainment/viewmodel/SignUpFragmentViewModel.kt
@@ -3,6 +3,7 @@ package com.example.myentertainment.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.myentertainment.BaseApplication
+import com.example.myentertainment.`object`.ValidationObject
 import com.google.firebase.auth.FirebaseAuth
 import javax.inject.Inject
 
@@ -12,23 +13,47 @@ class SignUpFragmentViewModel : ViewModel() {
         BaseApplication.baseApplicationComponent.inject(this)
     }
 
-    val signingUpStatus = MutableLiveData<Boolean>()
+    val validationResult = MutableLiveData<Int>()
     val loading = MutableLiveData<Boolean>()
+    val signingUpStatus = MutableLiveData<Boolean>()
 
     @Inject
     lateinit var databaseAuth: FirebaseAuth
 
     fun signUp(email: String, password: String) {
         loading.value = true
-        databaseAuth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener() { task ->
-                if (task.isSuccessful) {
-                    loading.value = false
-                    signingUpStatus.value = true
-                } else {
-                    loading.value = false
-                    signingUpStatus.value = false
+
+        if (validation(email, password)) {
+            databaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener() { task ->
+                    if (task.isSuccessful) {
+                        loading.value = false
+                        signingUpStatus.value = true
+                    } else {
+                        loading.value = false
+                        signingUpStatus.value = false
+                    }
                 }
-            }
+        }
+
+    }
+
+    private fun validation(email: String, password: String): Boolean {
+
+        if (email.isEmpty() || password.isEmpty()) {
+            loading.value = false
+            validationResult.value = ValidationObject.EMPTY_VALUES
+            return false
+        } else if (password.length < 6) {
+            loading.value = false
+            validationResult.value = ValidationObject.PASSWORD_TOO_SHORT
+            return false
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            loading.value = false
+            validationResult.value = ValidationObject.INVALID_EMAIL
+            return false
+        } else {
+            return true
+        }
     }
 }
