ff87768961a2e082a1e6cd750ec2f2c5da8d2942
natalia-mus <natalia.muskala1ab1@gmail.com>
2022-05-01 21:34:36 +0200

    SAVE

--- app/src/main/java/com/example/myentertainment/data/UserProfile.kt
+++ app/src/main/java/com/example/myentertainment/data/UserProfile.kt
@@ -2,8 +2,7 @@ package com.example.myentertainment.data
 
 data class UserProfile(
     val username: String? = null,
-    val firstName: String? = null,
-    val lastName: String? = null,
+    val realName: String? = null,
     val city: String? = null,
     val country: String? = null,
     val age: Int? = null
--- app/src/main/java/com/example/myentertainment/view/userprofile/UserProfileActivity.kt
+++ app/src/main/java/com/example/myentertainment/view/userprofile/UserProfileActivity.kt
@@ -1,6 +1,10 @@
 package com.example.myentertainment.view.userprofile
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -17,6 +21,14 @@ class UserProfileActivity : AppCompatActivity() {
     private lateinit var city: TextView
     private lateinit var country: TextView
     private lateinit var age: TextView
+    private lateinit var editButton: ImageButton
+    private lateinit var saveButton: Button
+
+    private lateinit var usernameEditable: EditText
+    private lateinit var realNameEditable: EditText
+    private lateinit var cityEditable: EditText
+    private lateinit var countryEditable: EditText
+    private lateinit var ageEditable: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,17 +43,67 @@ class UserProfileActivity : AppCompatActivity() {
         viewModel.userProfile.observe(this, { initView(it) })
     }
 
-    private fun initView(userProfileData: UserProfile) {
+    private fun initView(userProfileData: UserProfile?) {
         username = findViewById(R.id.userProfile_username)
         realName = findViewById(R.id.userProfile_realName)
         city = findViewById(R.id.userProfile_city)
         country = findViewById(R.id.userProfile_country)
         age = findViewById(R.id.userProfile_age)
+        editButton = findViewById(R.id.userProfile_buttonEdit)
+        saveButton = findViewById(R.id.userProfile_buttonSave)
 
-        username.text = userProfileData.username
-        realName.text = userProfileData.firstName.plus(" ").plus(userProfileData.lastName)
-        city.text = userProfileData.city
-        country.text = userProfileData.country
-        age.text = userProfileData.age.toString()
+        usernameEditable = findViewById(R.id.userProfile_username_editable)
+        realNameEditable = findViewById(R.id.userProfile_realName_editable)
+        cityEditable = findViewById(R.id.userProfile_city_editable)
+        countryEditable = findViewById(R.id.userProfile_country_editable)
+        ageEditable = findViewById(R.id.userProfile_age_editable)
+
+        editButton.setOnClickListener() {
+            prepareEditing()
+        }
+
+        saveButton.setOnClickListener() {
+            updateUserProfileData()
+        }
+
+        if (userProfileData != null) {
+            username.text = userProfileData.username
+            realName.text = userProfileData.realName
+            city.text = userProfileData.city
+            country.text = userProfileData.country
+            age.text = userProfileData.age.toString()
+
+            usernameEditable.setText(userProfileData.username)
+            realNameEditable.setText(userProfileData.realName)
+            cityEditable.setText(userProfileData.city)
+            countryEditable.setText(userProfileData.country)
+            ageEditable.setText(userProfileData.age.toString())
+        }
+    }
+
+    private fun prepareEditing() {
+        editButton.visibility = View.GONE
+        saveButton.visibility = View.VISIBLE
+        username.visibility = View.GONE
+        realName.visibility = View.GONE
+        city.visibility = View.GONE
+        country.visibility = View.GONE
+        age.visibility = View.GONE
+        usernameEditable.visibility = View.VISIBLE
+        realNameEditable.visibility = View.VISIBLE
+        cityEditable.visibility = View.VISIBLE
+        countryEditable.visibility = View.VISIBLE
+        ageEditable.visibility = View.VISIBLE
+    }
+
+    private fun updateUserProfileData() {
+        val username = usernameEditable.text.toString()
+        val realName = realNameEditable.text.toString()
+        val city = cityEditable.text.toString()
+        val country = countryEditable.text.toString()
+        val age = ageEditable.text.toString().toInt()
+
+        val userProfileData = UserProfile(username, realName, city, country, age)
+        viewModel.updateUserProfileData(userProfileData)
     }
 }
