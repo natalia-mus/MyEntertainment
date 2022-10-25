2869f73e2a567c863e0cd29e8a0beb4ca79f44dc
natalia-mus <natalia.muskala1ab1@gmail.com>
2022-10-25 22:51:05 +0200

    SAVE

---
+++ app/src/main/java/com/example/myentertainment/view/About.kt
@@ -0,0 +1,13 @@
+package com.example.myentertainment.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myentertainment.R
+
+class About : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.about)
+    }
+}
