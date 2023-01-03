2d991dd95ddaebf8265b087c5383b654a9ae5562
natalia-mus <natalia.muskala1ab1@gmail.com>
2023-01-03 18:37:53 +0100

    UUID instead of incremental id

--- app/src/main/java/com/example/myentertainment/viewmodel/problemreport/ProblemReportViewModel.kt
+++ app/src/main/java/com/example/myentertainment/viewmodel/problemreport/ProblemReportViewModel.kt
@@ -9,22 +9,18 @@ import com.example.myentertainment.`object`.ValidationResult
 import com.example.myentertainment.data.Date
 import com.example.myentertainment.data.ProblemReport
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Named
 
 class ProblemReportViewModel : ViewModel() {
 
     private var user: String = Constants.UNKNOWN_USER
-    private var itemId: String = "0"
 
     init {
         BaseApplication.baseApplicationComponent.inject(this)
         setUser()
-        setItemId()
     }
 
     @Inject
@@ -43,6 +39,7 @@ class ProblemReportViewModel : ViewModel() {
         loading.value = true
 
         if (validation(summary, description)) {
+            val itemId = UUID.randomUUID().toString()
             val report = ProblemReport(itemId, user, getDeviceModel(), getDeviceManufacturer(), getAndroidVersion(), summary, description, getDate())
 
             databaseReference.child(itemId).setValue(report).addOnCompleteListener() { task ->
@@ -74,25 +71,6 @@ class ProblemReportViewModel : ViewModel() {
         val date = Date(currentDateTime)
         date.setTime(currentDateTime)
         return date
-    }
-
-    private fun setItemId() {
-        databaseReference
-            .addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(error: DatabaseError) {}
-
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        val childrenCount = snapshot.childrenCount
-                        itemId = childrenCount.toString()
-
-                        for (i in 0 until childrenCount) {
-                            val child = snapshot.child(i.toString()).value
-                            if (child.toString() == Constants.NULL) itemId = i.toString()
-                        }
-                    }
-                }
-            })
     }
 
     private fun setUser() {
